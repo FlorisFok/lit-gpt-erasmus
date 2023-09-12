ee4c0edb39dee4b6a8481aaa80e139ea1e62fffe
FlorisFok <florisfok5@gmail.com>
2023-09-12 12:24:47 +0200

    edit

--- scripts/prepare_any.py
+++ scripts/prepare_any.py
@@ -62,7 +62,7 @@ def prepare(
         filename = destination_path / f"{split}.bin"
         dtype = np.uint16  # (can do since enc.max_token_value == 50256 is < 2**16)
         arr = np.memmap(str(filename), dtype=dtype, mode="w+", shape=(arr_len,))
-        total_batches = 1024
+        total_batches = dset["len"]
 
         idx = 0
         for batch_idx in tqdm(range(total_batches), desc=f"writing {filename}"):
